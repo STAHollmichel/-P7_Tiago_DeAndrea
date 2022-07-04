ec31695cbbe879e6ae9c8e4b1176a02f453f88ae
Tiago de Andrea <sta.holllmichel@gmail.com>
2022-07-04 17:25:44 +0100

    Backend: Sequelize Init [User : Migration & Model Customizations]

---
+++ backend/models/user.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+ 
+    }
+  };
+
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
+    password: {
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    profession: DataTypes.STRING,
+    profilePhoto: DataTypes.STRING,
+    bio: DataTypes.TEXT,
+    admin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+    }
+  }, 
+  {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
