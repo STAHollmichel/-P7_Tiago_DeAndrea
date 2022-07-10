5e9b5ae63bc10a005ecbb825443c3521a25bd0aa
Tiago de Andrea <sta.holllmichel@gmail.com>
2022-07-10 12:21:29 +0100

    Frontend: Footers [ warning error corrections ]

--- frontend/src/components/layout/footer.js
+++ frontend/src/components/layout/footer.js
@@ -5,30 +5,30 @@ function Footer() {
     return (
         <footer className='container-fluid text-center' id='home_footer'>
              <section className='text-center fs-3 py-4 bg-dark' id='social-icons'>
-                    <a className=" m-2" href="#!" role="button">
+                    <a className='m-2' href='#!' role='button'>
                         <FaFacebook />
                     </a>
-                    <a className=" m-2" href="#!" role="button">
+                    <a className='m-2' href='#!' role='button'>
                         <FaTwitter />    
                     </a>
-                    <a className=" m-2" href="#!" role="button">
+                    <a className='m-2' href='#!' role='button'>
                         <FaGoogle />
                     </a>
-                    <a className=" m-2" href="#!" role="button">
+                    <a className='m-2' href='#!' role='button'>
                         <FaInstagram />
                     </a>
-                    <a className=" m-2" href="#!" role="button">
+                    <a className='m-2' href='#!' role='button'>
                         <FaLinkedin />
                     </a>
                 </section>
                 <section className='text-center'>
                     <ul className='nav justify-content-center py-3' id='footer-links'>
-                        <li className='nav-item ps-3'><a href="#">Mentions légales</a></li>
-                        <li className='nav-item ps-3'><a href="#">Données personnelles</a></li>
-                        <li className='nav-item ps-3'><a href="#">Plan du site</a></li>
-                        <li className='nav-item ps-3'><a href="#">Contact</a></li>
-                        <li className='nav-item ps-3'><a href="#">Crédits</a></li>
-                        <li className='nav-item ps-3'><a href="#">Accessibilité</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Mentions légales</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Données personnelles</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Plan du site</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Contact</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Crédits</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Accessibilité</a></li>
                     </ul>
                     <div className='text-center'>
                         <p className='text-light py-3'>© Groupomania 2022</p>
--- frontend/src/components/layout/login_footer.js
+++ frontend/src/components/layout/login_footer.js
@@ -7,31 +7,31 @@ function LoginFooter() {
         <footer className='container-fluid bg-light' id='login_footer'>
                 <section className='text-center fs-3 py-4'>
                     <div className='pb-3'>
-                        <a className=' m-2' href='#!' role='button'>
+                        <a className='m-2' href='#!' role='button'>
                             <FaFacebook />
                         </a>
-                        <a className=' m-2' href='#!' role='button'>
+                        <a className='m-2' href='#!' role='button'>
                             <FaTwitter />    
                         </a>
-                        <a className=' m-2' href='#!' role='button'>
+                        <a className='m-2' href='#!' role='button'>
                             <FaGoogle />
                         </a>
-                        <a className=' m-2' href='#!' role='button'>
+                        <a className='m-2' href='#!' role='button'>
                             <FaInstagram />
                         </a>
-                        <a className=' m-2' href='#!' role='button'>
+                        <a className='m-2' href='#!' role='button'>
                             <FaLinkedin />
                         </a>
                     </div>
                 </section>
                 <section className='text-center'>
                     <ul className='nav justify-content-center py-3'>
-                        <li className='nav-item ps-3'><a href="#">Mentions légales</a></li>
-                        <li className='nav-item ps-3'><a href="#">Données personnelles</a></li>
-                        <li className='nav-item ps-3'><a href="#">Plan du site</a></li>
-                        <li className='nav-item ps-3'><a href="#">Contact</a></li>
-                        <li className='nav-item ps-3'><a href="#">Crédits</a></li>
-                        <li className='nav-item ps-3'><a href="#">Accessibilité</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Mentions légales</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Données personnelles</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Plan du site</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Contact</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Crédits</a></li>
+                        <li className='nav-item ps-3'><a href='#!' role='button'>Accessibilité</a></li>
                     </ul>
                     <div className='text-center'>
                         <p className='text-muted py-3'>© Groupomania 2022</p>
